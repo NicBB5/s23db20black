5a97fd0a21c1272e9a1b2e37b8858903b9b31683
Nic Black <78877662+NicBB5@users.noreply.github.com>
2023-04-20 12:54:54 -0500

    Completed part 1 of lab 13

--- routes/Jersey.js
+++ routes/Jersey.js
@@ -7,16 +7,15 @@ var router = express.Router();
 //   res.render('Jersey', { title: 'Search Results Jersey' });
 // });
 
-/* router.get('/', Jersey_controllers.Jersey_view_all_Page);
-module.exports = router; */
+router.get('/', Jersey_controllers.Jersey_view_all_Page);
 
-/* router.get('/Jersey/:id', Jersey_controllers.Jersey_detail); */
+router.get('/Jersey/:id', Jersey_controllers.Jersey_detail);
 
-/* router.get('/detail', Jersey_controllers.Jersey_view_one_Page); */
+router.get('/detail', Jersey_controllers.Jersey_view_one_Page);
 
-/* router.get('/create', Jersey_controllers.Jersey_create_Page); */
+router.get('/create', Jersey_controllers.Jersey_create_Page);
 
-/* router.get('/update', Jersey_controllers.Jersey_update_Page); */
+router.get('/update', Jersey_controllers.Jersey_update_Page);
 
 router.get('/delete', Jersey_controllers.Jersey_delete_Page);
 module.exports = router;
