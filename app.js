7e7965da78f9f77fdfa041a1b70815dd7a34be3f
Nic Black <78877662+NicBB5@users.noreply.github.com>
2023-04-20 13:14:10 -0500

    completed part 2 of lab 13

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(function(username, password, done){
+  Account.findOne({username: username}, function(err, user) {
+    if(err) {return done(err); }
+    if(!user) {
+      return done(null, false, {message: 'Incorrect username.'});
+    }
+    if(!user.validPassword(password)) {
+      return done(null, false, {message:'Incorrect password.'});
+    }
+    return done(null, user);
+  });
+}))
 
 var Jersey = require("./models/Jersey");
 
@@ -31,6 +46,11 @@ var JerseyRouter = require('./routes/Jersey')
 var boardRouter = require('./routes/board')
 var selectorRouter = require('./routes/selector')
 var resourceRouter = require("./routes/resource")
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 // view engine setup
@@ -41,6 +61,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret:'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
