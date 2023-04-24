66070bf9a8438e55d6e3f6a48fbc68d27fd9bd9d
Nic Black <78877662+NicBB5@users.noreply.github.com>
2023-04-24 12:49:19 -0500

    started part 4

--- routes/Jersey.js
+++ routes/Jersey.js
@@ -2,6 +2,14 @@ var express = require('express');
 const Jersey_controllers = require('../controllers/Jersey');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if(req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('Jersey', { title: 'Search Results Jersey' });
@@ -15,7 +23,7 @@ router.get('/detail', Jersey_controllers.Jersey_view_one_Page);
 
 router.get('/create', Jersey_controllers.Jersey_create_Page);
 
-router.get('/update', Jersey_controllers.Jersey_update_Page);
+router.get('/update', secured, Jersey_controllers.Jersey_update_Page);
 
 router.get('/delete', Jersey_controllers.Jersey_delete_Page);
 module.exports = router;
