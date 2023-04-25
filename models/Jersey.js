4660993125fe2b7ec0de72a8b8b4319819a2fb6a
Nic Black <78877662+NicBB5@users.noreply.github.com>
2023-04-25 12:27:40 -0500

    attempted part 6

--- models/Jersey.js
+++ models/Jersey.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const JerseySchema = mongoose.Schema({
     Jersey_type: String,
     size: String,
-    Jersey_number: Number
+    Jersey_number: {type: Number, min: 1, max: 99 }
 })
 
 module.exports = mongoose.model("Jersey", JerseySchema)
